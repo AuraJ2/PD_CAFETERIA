7d949ebeec11eec3d12aae1dfebccb3d203344a7
Reséndiz González José Emanuel <emanuelgonzalez@micorreo.upp.edu.mx>
2022-04-06 22:11:20 -0500

    Agregué Frm tarjeta

--- src/Vistas/FrmPagoTarjeta.java
+++ src/Vistas/FrmPagoTarjeta.java
@@ -17,7 +17,7 @@ public class FrmPagoTarjeta extends javax.swing.JFrame {
     public FrmPagoTarjeta() {
         initComponents();
     }
-
+    //Hola
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
